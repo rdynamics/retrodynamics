f6e5d563a1331a2500c0c103284f6c97ff15eb16
TheMonsterFromTheDeep <themonsterfromthedeep@gmail.com>
2017-11-30 10:27:07 -0700

    Implement entity conditional iteration This stuff is pretty cool.
    Basically, it's now possible to loop over some given set of entities and
    just perform operations on them. Right now there aren't a whole lot of
    selectors, but for example, it's possible to do something like this:
    
      entity *player = ...
      for_ent(e, ent_all(
          within_radius(player->position, 50),
          is_not(player)
          ) {
              player_shoot_towards(e);
          }
      )
    
    there should also be selectors for if an entity has a particular
    component or a particular tag (which is something that is unimplemented
    as of yet, but would basically just be an extra int field in the entity)

--- src/entity.c
+++ src/entity.c
@@ -1,9 +1,13 @@
 #include "entity.h"
 #include "new.h"
 
+#include <stdarg.h>
+
 zlist_of(entity*) entity_pool;
 
 size_t minimum_empty = 0;
+size_t min = 0;
+size_t max_plus_one = 0;
 
 void ent_init(void) {
     /* Start the entity pool with 100 entities */
@@ -32,10 +36,13 @@ entity *ent_new(void) {
         if(!entity_pool[i]) {
             entity_pool[i] = ent;
             minimum_empty = i + 1;
+            if(i < min) min = i;
+            if(i + 1 > max_plus_one) max_plus_one = i + 1;
             return ent;
         }
     }
-    minimum_empty = zsize(entity_pool);
+    max_plus_one = zsize(entity_pool) + 1;
+    minimum_empty = zsize(entity_pool) + 1;
     zlist_add(entity_pool, ent);
     return ent;
 }
@@ -43,14 +50,132 @@ entity *ent_new(void) {
 void ent_destroy(entity *e) {
     if(!e) return;
     
+    int is_min = 1;
+    
     for(size_t i = 0; i < zsize(entity_pool); ++i) {
         if(entity_pool[i] == e) {
-            minimum_empty = i;
+            if(i < minimum_empty) minimum_empty = i;
             entity_pool[i] = NULL;
+            /* the new minimum will be the entity immediately after this one, because
+             * now this one will be null
+             */
+            if(is_min) min = i + 1;
+            /* the new maximum can simply be stepped back, because now this one is null
+             */
+            if(i + 1 == max_plus_one) --max_plus_one;
             break;
-        }
+        } else if(entity_pool[i]) is_min = 0;
     }
     
     zfree(e->components);
     free(e);
+}
+
+entity *ent_all_next(void *data) {
+    size_t i = *(size_t*)(data);
+    if(i >= max_plus_one) return NULL;
+    
+    size_t count = *(size_t*)(data + sizeof(size_t));
+    ent_condition *list = (ent_condition*)(data + 2 * sizeof(size_t));
+    
+    //ent_condition c = *(ent_condition*)(data + sizeof(size_t));
+    
+    size_t current = i;
+    ++i;
+    for(;;) {
+        if(i >= max_plus_one) break;
+        if(entity_pool[i]) {
+            for(size_t j = 0; j < count; ++j) {
+                if(!list[j].compatible(entity_pool[i], list[j].data)) {
+                    goto incompatible;
+                }
+            }
+            break;
+            incompatible: { }
+        }
+        ++i;
+    }
+    *(size_t*)(data) = i;
+    return entity_pool[current];
+}
+
+void ent_all_cleanup(void *data) {
+    size_t count = *(size_t*)(data + sizeof(size_t));
+    ent_condition *list = (ent_condition*)(data + 2 * sizeof(size_t));
+    for(size_t i = 0; i < count; ++i) {
+        list[i].cleanup(list[i].data);
+    }
+    free(data);
+}
+
+int ent_all_has_next(void *data) {
+    size_t i = *(size_t*)(data);
+    return i != zsize(entity_pool);
+}
+
+ent_iterator ent_all_(size_t len, ...) {
+    va_list args;
+    va_start(args, len);
+    
+    void *data = malloc(2 * sizeof(size_t) + len);
+    
+    size_t count = 0;
+    
+    ent_condition *list = (ent_condition*)(data + 2 * sizeof(size_t));
+    while(len) {
+        list[count] = va_arg(args, ent_condition);
+        ++count;
+        len -= sizeof(ent_condition);
+    }
+    
+    *(size_t*)(data + sizeof(size_t)) = count;
+    
+    size_t i = min;
+    for(;;) {
+        if(entity_pool[i]) {
+            for(size_t j = 0; j < count; ++j) {
+                if(!list[j].compatible(entity_pool[i], list[j].data)) {
+                    goto incompatible;
+                }
+            }
+            break;
+            incompatible: { }
+        }
+        ++i;
+        if(i == zsize(entity_pool)) break;
+    }
+    
+    *(size_t*)(data) = i;
+    
+    return (ent_iterator){ &ent_all_next, /*&ent_all_has_next,*/ &ent_all_cleanup, data };
+}
+
+void radius_cleanup(void *data) {
+    free(data);
+}
+
+int radius_compatible(entity *e, void *data) {
+    vec *v = (vec*)data;
+    float *r = (float*)(data + sizeof(vec));
+    float test = len2(vsub(e->position, *v));
+    return test <= (*r) * (*r);
+}
+
+ent_condition within_radius(vec v, float r) {
+    void *data = malloc(sizeof(vec) + sizeof(float));
+    *(vec*)(data) = v;
+    *(float*)(data + sizeof(vec)) = r;
+    
+    return (ent_condition){ &radius_compatible, &radius_cleanup, data };
+}
+
+void is_not_cleanup(void *data) { }
+
+int is_not_compatible(entity *e, void *data) {
+    entity *check = (entity*)data;
+    return check != e;
+}
+
+ent_condition is_not(entity *e) {
+    return (ent_condition){ &is_not_compatible, &is_not_cleanup, e };
 }
--- src/entity.h
+++ src/entity.h
@@ -14,4 +14,31 @@ typedef struct {
 extern entity *ent_new(void);
 extern void ent_destroy(entity*);
 
+typedef struct {
+    entity *(*next)(void*);
+    void (*cleanup)(void*);
+    void* data;
+} ent_iterator;
+
+typedef struct {
+    int (*compatible)(entity*,void*);
+    void (*cleanup)(void*);
+    void *data;
+} ent_condition;
+
+ent_condition within_radius(vec,float);
+ent_condition is_not(entity*);
+
+ent_iterator ent_all_(size_t,...);
+
+#define ent_all(...) ent_all_(sizeof((ent_condition[]){ __VA_ARGS__ }), __VA_ARGS__)
+
+#define for_ent(name,it,k) {\
+    ent_iterator name ## _iterator = it;\
+    entity *name = name ## _iterator.next(name ## _iterator.data);\
+    for(; name; name = name ## _iterator.next(name ## _iterator.data))\
+        k\
+    name ## _iterator.cleanup(name ## _iterator.data);\
+}
+
 #endif
